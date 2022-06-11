46d31012f79072b285fbe82f859fb509bbfc8f16
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-12 02:05:31 +0300

    week3 day4  flask

---
+++ python/week3/day4/HelloFlask.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo"
+
+
+@app.route('/say/<name>')
+def hello(name):
+    return "Hi " + name
+
+
+@app.route('/repeat/<int:number>/<word>')
+def repeat(number, word):
+    text = ""
+    for i in range(int(number)):
+        text += word
+    return text
+
+@app.errorhandler(404)
+def pageNotFound(e):
+    return "Sorry! No response. Try again"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
