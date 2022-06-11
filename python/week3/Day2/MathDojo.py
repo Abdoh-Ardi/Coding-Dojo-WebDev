28ae1da0ea9cccce99e2704c236eecb5d4d2d84a
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-11 23:07:56 +0300

    week 3 day 2

--- python/week3/Day2/MathDojo.py
+++ python/week3/Day2/MathDojo.py
@@ -22,10 +22,12 @@ print(x)	# should print 5
 # run each of the methods a few more times and check the result!
 x = md.add(2).result
 print(x)
-x = md.add(2,5,1).subtract(3,2).result
+x = md.add(6,7,9).subtract(1,5).result
 print(x)
-x = md.subtract(2).result
+x = md.subtract(9).result
 print(x)
-x = md.subtract(3,2).result
+x = md.subtract(1,1).result
+print(x)
+x = md.add(6,7,9).subtract(1,5).result
 print(x)
 
