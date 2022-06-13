a40f5d5aff9ee64b5a96125664d48a2313d0972e
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-13 07:41:53 +0300

    week 3 day 4 checkerboard

--- python/week3/day4/checkerboard.py
+++ python/week3/day4/checkerboard.py
@@ -1,17 +1,27 @@
 from ctypes import sizeof
 from flask import Flask, render_template
 app = Flask(__name__)
+box = ""
+counter = 0
 
 
 @app.route('/')
 def index():
+    
+    return render_template('checkerboard.html', row=1,column=4)
 
-    return render_template('checkerboard.html',size=1,toggle=True)
+@app.route('/<x>')
+def xRows(x):
+    return render_template('checkerboard.html', row=int(x),column=int(x))
+
+@app.route('/<x>/<y>')
+def xRowsyColumns(x,y):
+    return render_template('checkerboard.html', row=int(x),column=int(y))
 
 if __name__ == '__main__':
-    app.static_folder = 'static' 
+    app.static_folder = 'static'
     app.run(debug=True)
- 
+
 #  <!-- based on the folder structure on the right -->
 # <!-- linking a css style sheet -->
 # <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='my_style.css') }}">
@@ -19,4 +29,3 @@ if __name__ == '__main__':
 # <script type="text/javascript" src="{{ url_for('static', filename='my_script.js') }}"></script>
 # <!-- linking an image -->
 # <img src="{{ url_for('static', filename='my_img.png') }}">
-
