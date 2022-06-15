59a1a3248ca02633d9bc12e13c901eb91cd70d89
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-15 11:27:56 +0300

    week 4 day 4

--- python/django/django_fundamentals/djangoIntro/djangoTutorial/urls.py
+++ python/django/django_fundamentals/djangoIntro/djangoTutorial/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path     
-from . import views
+from . import views	# the . indicates that the views file can be found in the same directory as this file
 urlpatterns = [
     path('', views.index),
+    path('bears', views.bearsMethod),
+    path('bears/<int:value>',views.bearsNumberMethod),
+    path('bears/<str:name/poke',views.bearNameMethod),
+    path('<int:id>/<str:color>',views.numberAndColor)
 ]
--- python/django/django_fundamentals/djangoIntro/djangoTutorial/views.py
+++ python/django/django_fundamentals/djangoIntro/djangoTutorial/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("this is the equivalent of @app.route('/')!")
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse
+
+
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return HttpResponse("response from index method from root route, localhost:8000!")
+
+def bearsMethod(request):
+    pass
+
+def bearsNumberMethod(request, value):
+    pass
+
+def bearNameMethod(request, name):
+    pass
+
+def numberAndColor(request, value, color):
+    pass
+
