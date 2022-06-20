221d80b1d9faedeb90ae9a32d222c8226588b55c
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-20 14:51:42 +0300

    django orm changes

---
+++ python/django/django_orm/djangoORM/usersShell/models.py
@@ -0,0 +1,27 @@
+import email
+from operator import truediv
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
