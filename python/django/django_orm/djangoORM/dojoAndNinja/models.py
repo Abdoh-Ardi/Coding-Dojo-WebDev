f46b8a05604ed8e77b23dcb3cacb531d85dc6081
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-27 02:01:18 +0300

    django ORM dojo&ninjas query

---
+++ python/django/django_orm/djangoORM/dojoAndNinja/models.py
@@ -0,0 +1,21 @@
+
+from operator import mod
+from django.db import models
+
+# Create your models here.
+
+
+class Dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField(default="old dojo")
+
+
+class Ninjas(models.Model):
+    dojo_id = models.IntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(
+        Dojos, related_name="ninjas", on_delete=models.CASCADE)
+
