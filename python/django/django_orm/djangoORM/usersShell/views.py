f7b6c838e8c029c41deaae486221b5dce79b360e
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-26 10:49:36 +0300

    django User modified

--- python/django/django_orm/djangoORM/usersShell/views.py
+++ python/django/django_orm/djangoORM/usersShell/views.py
@@ -1,16 +1,21 @@
 from collections import UserList
 import email
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import User
 
 # Create your views here.
 
 
 def index(request):
-    if request.method == "POST":
-       User.objects.create(first_name= request.POST['FirstName'],last_name=request.POST['LastName'],email=request.POST['Email'],age=request.POST['Age'])
     context = {
         "usersList": User.objects.all()
     }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
+
+
+def form(request):
+    if request.method == "POST":
+        User.objects.create(
+            first_name=request.POST['FirstName'], last_name=request.POST['LastName'], email=request.POST['Email'], age=request.POST['Age'])
+    return redirect('/userShell')
