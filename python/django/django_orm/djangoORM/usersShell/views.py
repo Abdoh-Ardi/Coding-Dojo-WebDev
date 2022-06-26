2b6295813884bb35aa7c200b014ca7ea563ff1f7
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-06-26 10:30:49 +0300

    django User

--- python/django/django_orm/djangoORM/usersShell/views.py
+++ python/django/django_orm/djangoORM/usersShell/views.py
@@ -1,7 +1,16 @@
+from collections import UserList
+import email
+from multiprocessing import context
 from django.shortcuts import render
 from .models import User
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,'index.html')
+    if request.method == "POST":
+       User.objects.create(first_name= request.POST['FirstName'],last_name=request.POST['LastName'],email=request.POST['Email'],age=request.POST['Age'])
+    context = {
+        "usersList": User.objects.all()
+    }
+    return render(request, 'index.html',context)
---
+++ python/wekk6/day1/morningAlgo.js
@@ -0,0 +1,44 @@
+
+function recurse(num) {
+    if (num > 1) {
+        console.log(num)
+        return recurse(num - 1);
+    }
+    return 1;
+}
+
+console.log(recurse(10));
+
+
+let arr = [];
+// console.log(recursArr(arr, 10));
+console.log(recursArr(arr, 10));
+
+
+
+
+
+// function recursArr(arr, num) {
+//     if (arr.length >= 10) {
+//         return arr;
+//     } else {
+//         arr.push(num)
+//         return recursArr(arr, num - 1)
+//     }
+// }
+
+function recursArr(arr, num) {
+    if (num < 1) {
+        return arr;
+    } else {
+        arr.push(num);
+        return recursArr(arr, num - 1);
+    }
+}
+
+
+// function recursWithoutArr(num) {
+//     if (num) {
+
+//     }
+// }
