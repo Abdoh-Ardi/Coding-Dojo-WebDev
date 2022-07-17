16cb98844b90c39e6517fb02d8fe96541b3e0ec8
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-07-17 11:51:08 +0300

    first java program

---
+++ java/JavaFundamentals/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+package JavaFundamentals;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Abdoh Ardi\nI am 24 years old\nMy hometown is Jeddah");
+        
+    }
+}
