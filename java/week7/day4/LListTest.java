440aa98c4f549ece0358a2ca2d3de61c37dfc005
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-07-21 09:19:43 +0300

    LinkedList

--- java/week7/day4/LListTest.java
+++ java/week7/day4/LListTest.java
@@ -4,16 +4,19 @@ package week7.day4;
 
 public class LListTest {
     public static void main(String[] args) {
+        //single linked list test
         LList list = new LList();
-
         list.add(new Node("value1"));
         list.add(new Node("value2"));
         list.add(new Node("value3"));
         list.add(new Node("value4"));
         list.addFirst(new Node("value0"));
         // list.removeFirst();
-        list.removeAt(3);
-        System.out.println(list.get(1).getValue());
+        list.removeAt(3);//remove the value3
         list.displayAll();
+        System.out.println("get value at index 1: "+list.get(1).getValue());
+
+
+        //double linkedlist test
     }
 }
