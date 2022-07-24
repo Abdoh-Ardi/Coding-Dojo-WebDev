2af05b3a45e8bbe6ac1f065591e65c33526504f2
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-07-24 14:05:09 +0300

    Daikichi added path variables

--- java/stsWorkSpace/demo1/src/main/java/com/example/demo/Daikichi/DaikichiController.java
+++ java/stsWorkSpace/demo1/src/main/java/com/example/demo/Daikichi/DaikichiController.java
@@ -3,6 +3,7 @@ package com.example.demo.Daikichi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -23,5 +24,16 @@ public class DaikichiController {
     public String tomorrowMsg() {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
     }
+    // http://localhost:8080/daikichi/travel/china
+    @RequestMapping("/travel/{cityName}")
+    public String travelMsg(@PathVariable(name = "cityName") String cityName) {
+        return "Congratulations! You will soon travel to " + cityName + "!";
 
+    }
+    // http://localhost:8080/daikichi/lotto/1
+    @RequestMapping("/lotto/{number}")
+    public String lottoMsg(@PathVariable(name = "number") int number) {
+        return number % 2 == 0 ? "You will take a grand journey in the near future, but be weary of tempting offers"
+                : "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    }
 }
