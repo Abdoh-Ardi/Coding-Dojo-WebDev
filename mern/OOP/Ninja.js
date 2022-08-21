b43abf9dd13e4d1f781d4f8f4638713c11dc73f5
Abdoh-Ardi <abdoh.ardi3@gmail.com>
2022-08-21 05:44:51 +0300

    super ninja

--- mern/OOP/Ninja.js
+++ mern/OOP/Ninja.js
@@ -1,4 +1,3 @@
-
 class Ninja {
     constructor(name, health) {
         this.name = name;
@@ -26,3 +25,27 @@ class Ninja {
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        super.health = 200;
+        super.speed = 10;
+        super.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        super.drinkWater();
+    }
+
+
+}
+
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
