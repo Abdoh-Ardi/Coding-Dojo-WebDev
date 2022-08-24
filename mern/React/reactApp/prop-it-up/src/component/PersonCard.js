93dedf5726277e5ca5cdf3ec8e4529a4709be7aa
Abdoh Ardi <abdoh.ardi3@gmail.com>
2022-08-24 15:55:12 +0300

    putting it together

--- mern/React/reactApp/prop-it-up/src/component/PersonCard.js
+++ mern/React/reactApp/prop-it-up/src/component/PersonCard.js
@@ -2,16 +2,26 @@ import "./PersonCard.css";
 // import './App.css';
 const { Component } = require("react");
 class PersonCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            Age: props.age,
+        };
+
+
+    }
+    incrementAge = () => { this.setState({ Age: this.state.Age + 1 }) };
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
         return (
             <>
                 <div className="center">
                     <h1>{firstName},{lastName}</h1>
-                    <p>Age: {age}</p>
+                    <p>Age: {this.state.Age}</p>
                     <p>Hair Color: {hairColor}</p>
+                    <button onClick={this.incrementAge}>BirthDay Button for {firstName} {lastName}</button>
                 </div>
-
             </>
 
         );
