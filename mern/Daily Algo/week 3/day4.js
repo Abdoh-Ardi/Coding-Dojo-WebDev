cd3ccf117ed2cc940dd35f51759435379715744b
Abdoh Ardi <abdoh.ardi3@gmail.com>
2022-08-31 10:39:09 +0300

    day4 funnyString

--- mern/Daily Algo/week 3/day4.js
+++ mern/Daily Algo/week 3/day4.js
@@ -47,8 +47,8 @@
 function funnyStringOptimized(str) {
 
     for (let i = 0; i < str.length / 2; i++) {
-        resultLeftSide = Math.abs(str.charCodeAt(i) - str.charCodeAt(i + 1)) == 1;
-        resultRightSide = Math.abs(str.charCodeAt(str.length - 1 - i) - str.charCodeAt(str.length - 2 - i)) == 1;
+        let resultLeftSide = Math.abs(str.charCodeAt(i) - str.charCodeAt(i + 1)) == 1;
+        let resultRightSide = Math.abs(str.charCodeAt(str.length - 1 - i) - str.charCodeAt(str.length - 2 - i)) == 1;
         if (resultRightSide == resultLeftSide) {
             continue;
         } else {
@@ -61,7 +61,6 @@ function funnyStringOptimized(str) {
 
 console.log(funnyStringOptimized("ponml") == "Funny");
 console.log(funnyStringOptimized("lmnop") == "Funny");
-
 console.log(funnyStringOptimized("abcd") == "Funny");
 console.log(funnyStringOptimized("xyzed") == "notFunny");
 console.log(funnyStringOptimized("bcxz") == "notFunny");
